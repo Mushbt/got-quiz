707b5b1bf1f9303f4524e62106ed8e93c3ceb85d
Mushbt <mustafa_habet@hotmail.com>
2022-11-26 09:34:14 +0000

    Add variables to game js file

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,9 @@
 const question = document.querySelector('#question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
 console.log(choices);
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
