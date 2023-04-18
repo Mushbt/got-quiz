f58a7285211a88ecccb85010d3b5fe2d14baa022
Mushbt <mustafa_habet@hotmail.com>
2023-04-18 11:14:20 +0000

    Add comments describing the functions for the end game

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,4 +1,8 @@
 /*jshint esversion: 6 */
-const finalScore = document.getElementById('finalScore');
-const mostRecentScore = localStorage.getItem('mostRecentScore'); //Get your final score
+// Declares a constant variable finalScore and assigns it the value of the DOM element with the ID of finalScore using the document.getElementById() method.
+const finalScore = document.getElementById('finalScore'); 
+// Declares a constant variable mostRecentScore and assigns it the value of the most recent score from the local storage using the localStorage.getItem() method.
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+// Sets the text of the finalScore element to display the final score using template literals, with the text "Your Score: " followed by the value of the mostRecentScore variable.
+// This is done using the innerText property of the finalScore element.
 finalScore.innerText = `Your Score: ${mostRecentScore}`;
